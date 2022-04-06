c34232057873a55a6d2c54bc9dff908dc07901da
gabrielemigliorinii <gabrieledevs@gmail.com>
2022-04-06 22:06:06 +0200

    Added IMG_CAP.py file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 
 if __name__ == '__main__':
-    img = "img.jpg"
-    tool = "IMG_READ_SHOW_WRITE"
-    os.system("python src/{}.py --image img/{}".format(tool, img))
+    #img = "img.jpg"  --image img/{}
+    tool = "IMG_CAP"
+    os.system("python src/{}.py".format(tool))
