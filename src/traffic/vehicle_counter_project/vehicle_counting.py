0d80dc572294ca901bf5cc483ebf9db4f4dd1a37
gabrielemigliorinii <97559996+gabrielemigliorinii@users.noreply.github.com>
2022-06-26 23:03:14 +0200

    Update vehicle_counting.py

--- src/traffic/vehicle_counter_project/vehicle_counting.py
+++ src/traffic/vehicle_counter_project/vehicle_counting.py
@@ -1,12 +1,11 @@
 import cv2
-import glob
 import os
 from vehicle_detector import VehicleDetector
 
 def program():
     vd = VehicleDetector()
     vehicles_folder_count = 0
-    img = cv2.imread("images/18.jpg")
+    img = cv2.imread("images/6.jpg")
     vehicle_boxes = vd.detect_vehicles(img)
     vehicle_count = len(vehicle_boxes)
 
@@ -15,10 +14,10 @@ def program():
     for box in vehicle_boxes:
         x, y, w, h = box
 
-        cv2.rectangle(img, (x, y), (x + w, y + h), (25, 0, 180), 3)
-        cv2.putText(img, "Vehicles: " + str(vehicle_count), (20, 50), 0, 2, (100, 200, 0), 3)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3)
+        cv2.putText(img, "VEHICLES DETECTED: " + str(vehicle_count), (20, 50), 0, 2, (0, 0, 0), 3)
 
-    cv2.imshow("Cars", img)
+    cv2.imshow("CARS", img)
     cv2.waitKey(1)
 
     os.system("PAUSE")
