b1b21e86f92c2ff4e4506d2a85d330025e97cdab
gabrielemigliorinii <97559996+gabrielemigliorinii@users.noreply.github.com>
2022-06-26 23:03:57 +0200

    Update and rename FACE_DETECTION_MOD.py to FACE_DETECTION_ADV.py

--- src/face/FACE_DETECTION_ADV.py
+++ src/face/FACE_DETECTION_ADV.py
@@ -25,8 +25,8 @@ class FaceDetector:
                 bbox = [int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)]
                 if draw:
                     img = self.drawLines(img,bbox)
-                    cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,2, (255, 0, 255), 2)
-                    bbox = [["X",int(bboxC.xmin * iw)], ["Y",int(bboxC.ymin * ih)], ["WIDTH",int(bboxC.width * iw)], ["HEIGHT",int(bboxC.height * ih)]]
+                    cv2.putText(img, f'{int(detection.score[0] * 100)}%', (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,2, (255, 255, 0), 2)
+                    bbox = [[int(bboxC.xmin * iw)], [int(bboxC.ymin * ih)], [int(bboxC.width * iw)], [int(bboxC.height * ih)]]
                     bboxs.append([["FACE_ID", id], bbox, ["DETECTION_SCORE", detection.score[0]]])
 
             return img, bboxs
@@ -37,24 +37,30 @@ class FaceDetector:
     def drawLines(self, img, bbox, l=30, t=5, rt= 1):
         x, y, w, h = bbox
         x1, y1 = x + w, y + h
-        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        cv2.rectangle(img, bbox, (255, 255, 0), rt)
         # Top Left  x,y
-        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
-        cv2.line(img, (x, y), (x, y+l), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x + l, y), (255, 255, 0), t)
+        cv2.line(img, (x, y), (x, y+l), (255, 255, 0), t)
         # Top Right  x1,y
-        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
-        cv2.line(img, (x1, y), (x1, y+l), (255, 0, 255), t)
-        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
-        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
-        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
-        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 255, 0), t)
+        cv2.line(img, (x1, y), (x1, y+l), (255, 255, 0), t)
+        cv2.line(img, (x, y1), (x + l, y1), (255, 255, 0), t)
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 255, 0), t)
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 255, 0), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 255, 0), t)
         return img
 
 def printDetectionArray(array):
     print(array)
 
+def rescaleFrame(frame, percent=50):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
 def main():
-    cap = cv2.VideoCapture("../vid/mix.mov")
+    cap = cv2.VideoCapture("../../vid/face_det_1.mp4")
     pTime = 0
     detector = FaceDetector()
     while True:
@@ -64,8 +70,8 @@ def main():
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-        cv2.imshow("Image", img)
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 2)
+        cv2.imshow("FRAME", rescaleFrame(img))
         cv2.waitKey(1)
 
 if __name__ == "__main__":
